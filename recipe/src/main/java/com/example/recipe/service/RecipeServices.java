82e30de58d37ed2fade89fbccf687a4dddbb69cd
MarisHeinols <73535385+MarisHeinols@users.noreply.github.com>
2021-11-10 12:55:30 +0200

    Fixed RecipeServices bean to Service

--- recipe/src/main/java/com/example/recipe/service/RecipeServices.java
+++ recipe/src/main/java/com/example/recipe/service/RecipeServices.java
@@ -2,7 +2,9 @@ package com.example.recipe.service;
 
 import com.example.recipe.entity.Recipe;
 import com.example.recipe.repository.RecipesRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RecipeServices {
 
     public final RecipesRepository recipesRepository;
